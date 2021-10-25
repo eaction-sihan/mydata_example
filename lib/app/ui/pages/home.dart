bb7b3d06e6abbc5cd5efac79a2bfa745e6861b0e
HanSeungil2 <ouze@naver.com>
2021-10-25 10:47:30 +0900

    7

--- lib/app/ui/pages/home.dart
+++ lib/app/ui/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_pattern/app/controller/home_controller.dart';
@@ -10,37 +12,85 @@ import 'package:getx_pattern/app/ui/widgets/left_menu.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return GetBuilder<HomeController>(
       init: HomeController(),
       builder: (_) {
-        return Scaffold(
-            appBar: AppBar1(),
-            drawer: LeftMenu1(),
-            backgroundColor: Colors.white,
-            body: SafeArea(
-              child: IndexedStack(
-                index: _.tabIndex,
-                children: <Widget>[
-                  Tab1(),
-                  Tab2(),
-                  Container(
-                    child: Center(
-                      child: Text('save'),
-                    ),
+        return new WillPopScope(
+            onWillPop: () => showExitPopup(context),
+            child: Scaffold(
+                appBar: AppBar1(),
+                drawer: LeftMenu1(),
+                backgroundColor: Colors.white,
+                body: SafeArea(
+                  child: IndexedStack(
+                    index: _.tabIndex,
+                    children: <Widget>[
+                      Tab1(),
+                      Tab2(),
+                      Container(
+                        child: Center(
+                          child: Text('save'),
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text('more'),
+                        ),
+                      ),
+                    ],
                   ),
-                  Container(
-                    child: Center(
-                      child: Text('more'),
-                    ),
-                  ),
+                ),
+                bottomNavigationBar: BottomBar(),
+                floatingActionButton: FloatingActionButton(
+                    child: Icon(Icons.add), onPressed: _.increment)));
+      },
+    );
+  }
+
+  Future<bool> showExitPopup(context) async {
+    return await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Container(
+              height: 90,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("종료하시겠습니까?"),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print('yes selected');
+                            exit(0);
+                          },
+                          child: Text("Yes"),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.blue.shade800),
+                        ),
+                      ),
+                      SizedBox(width: 15),
+                      Expanded(
+                          child: ElevatedButton(
+                        onPressed: () {
+                          print('no selected');
+                          Navigator.of(context).pop();
+                        },
+                        child:
+                            Text("No", style: TextStyle(color: Colors.black)),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                        ),
+                      ))
+                    ],
+                  )
                 ],
               ),
             ),
-            bottomNavigationBar: BottomBar(),
-            floatingActionButton: FloatingActionButton(
-                child: Icon(Icons.add), onPressed: _.increment));
-      },
-    );
+          );
+        });
   }
 }

30fd5db81825a6b787bf8a9d68bb93ff25da75fe
Han Seungil <sihan@e-act.co.kr>
2021-10-30 17:20:08 +0900

    10/30

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 
 import 'package:getx_pattern/app/ui/pages/menu1/menu1_main.dart';
+import 'package:getx_pattern/app/ui/pages/menu1/tab5/writing/writting.dart';
 import 'package:getx_pattern/app/ui/pages/menu2/menu2_main.dart';
 import 'package:getx_pattern/app/ui/widgets/splash/splash_screen.dart';
 part './app_routes.dart';
@@ -14,6 +15,10 @@ class AppPages {
     GetPage(
         name: Routes.MENU1,
         page: () => Menu1Main(),
+    ),
+    GetPage(
+      name: Routes.MENU1_TAB5_WRITING,
+      page: () => Menu1Tab5Writing(),
     ),
     GetPage(
         name: Routes.MENU2,
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,5 +4,7 @@ abstract class Routes{
 
   static const SPLASH = '/splash';
   static const MENU1 = '/menu1';
+  static const MENU1_TAB5_WRITING = '/menu1_tab5_writing';
   static const MENU2 = '/menu2';
+
 }

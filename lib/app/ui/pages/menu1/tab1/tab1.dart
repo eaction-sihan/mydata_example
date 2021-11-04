a4e201b59914c58c6c80f80fde6e7f7cdd05ebf7
HanSeungil2 <ouze@naver.com>
2021-11-04 14:03:48 +0900

    11/04

--- lib/app/ui/pages/menu1/tab1/tab1.dart
+++ lib/app/ui/pages/menu1/tab1/tab1.dart
@@ -4,12 +4,12 @@ import 'package:getx_pattern/app/ui/pages/menu1/tab1/tab1_1.dart';
 import 'package:getx_pattern/app/ui/pages/menu1/tab1/tab1_2.dart';
 import 'package:getx_pattern/app/ui/pages/menu1/tab1/tab1_3.dart';
 import 'package:getx_pattern/app/ui/theme/app_theme.dart';
+import 'package:getx_pattern/app/ui/widgets/modal/bottom_modal.dart';
 import 'package:getx_pattern/app/ui/widgets/tab/top_tab.dart';
 
 class Tab1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     List<String> categories = ["전체", "사회", "경제", "문화/연예", "제품", "건강", "기타"];
 
     return DefaultTabController(
@@ -18,23 +18,28 @@ class Tab1 extends StatelessWidget {
         children: <Widget>[
           Container(
             alignment: Alignment.centerLeft,
-            padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-                border:
-                    Border(bottom: BorderSide(width: 0.6, color: Colors.grey)),
-                color: Colors.white),
-            child: Row(children: [
-              Text(
-                'Agenda',
-                style: textTheme().headline1,
-              ),
-              SizedBox(width: 5),
-              Icon(
-                FontAwesomeIcons.chevronCircleDown,
-                color: fPrimaryColor,
-                size: 16,
-              ),
-            ]),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            decoration:
+                BoxDecoration(border: Border(bottom: BorderSide(width: 0.6, color: Colors.grey)), color: Colors.white),
+            child: TextButton(
+              onPressed: () {
+                BottomModal(cont: _bottomModal(context)).show(context);
+              },
+              child: Row(children: [
+                Text(
+                  'Agenda',
+                  style: textTheme().headline1,
+                ),
+                SizedBox(width: 5),
+                Icon(
+                  FontAwesomeIcons.chevronCircleDown,
+                  color: fPrimaryColor,
+                  size: 16,
+                ),
+              ]),
+            ),
           ),
           TopTab(categories: categories, isScroll: true),
           Expanded(
@@ -66,4 +71,24 @@ class Tab1 extends StatelessWidget {
       ),
     );
   }
+}
+
+Container _bottomModal(context) {
+  return Container(
+    height: 300,
+    color: Colors.white,
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const Text('Modal BottomSheet'),
+          ElevatedButton(
+            child: const Text('Close BottomSheet'),
+            onPressed: () => Navigator.pop(context),
+          )
+        ],
+      ),
+    ),
+  );
 }
